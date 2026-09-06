86c19da3a5ff32bde67bb30e28cc4fb7e3c6185d
joseslavkis <joseslavkis@gmail.com>
2026-09-06 20:48:23 -0300

    consumer logic for queue middleware

--- golang/internal/factory/queue_middleware.go
+++ golang/internal/factory/queue_middleware.go
@@ -9,12 +9,16 @@ import (
 )
 
 type QueueMiddleware struct {
-	queueName  string
-	connection *amqp.Connection
-	publisher  *amqp.Channel
-
-	mutex  sync.Mutex
-	closed bool
+	queueName        string
+	connection       *amqp.Connection
+	publisher        *amqp.Channel
+	consumer         *amqp.Channel
+	consumerTag      string
+	consumerSequence uint64
+	mutex         sync.Mutex
+	consumerMutex sync.Mutex
+	closed        bool
+	consuming     bool
 }
 
 func newQueueMiddleware(queueName string, connectionSettings m.ConnSettings) (m.Middleware, error) {
@@ -59,11 +63,120 @@ func (queue *QueueMiddleware) connect(connectionSettings m.ConnSettings) error {
 	return nil
 }
 
-func (queue *QueueMiddleware) StartConsuming(func(m.Message, func(), func())) error {
+func (queue *QueueMiddleware) StartConsuming(callback func(m.Message, func(), func())) error {
+	consumer, deliveries, err := queue.startConsumer()
+	if err != nil {
+		return err
+	}
+	defer queue.releaseConsumer(consumer)
+
+	return queue.consumeDeliveries(deliveries, callback)
+}
+
+func (queue *QueueMiddleware) startConsumer() (*amqp.Channel, <-chan amqp.Delivery, error) {
 	queue.mutex.Lock()
 	defer queue.mutex.Unlock()
 
-	if queue.closed {
+	if queue.closed || queue.connection.IsClosed() {
+		return nil, nil, m.ErrMessageMiddlewareDisconnected
+	}
+	if queue.consuming {
+		return nil, nil, m.ErrMessageMiddlewareMessage
+	}
+
+	consumer, err := queue.connection.Channel()
+	if err != nil {
+		return nil, nil, queue.consumptionError()
+	}
+
+	queue.consumerSequence++
+	consumerTag := fmt.Sprintf("consumer-%d", queue.consumerSequence)
+	deliveries, err := consumer.Consume(queue.queueName, consumerTag, false, false, false, false, nil)
+	if err != nil {
+		_ = consumer.Close()
+		return nil, nil, queue.consumptionError()
+	}
+
+	queue.consumer = consumer
+	queue.consumerTag = consumerTag
+	queue.consuming = true
+	return consumer, deliveries, nil
+}
+
+func (queue *QueueMiddleware) releaseConsumer(consumer *amqp.Channel) {
+	queue.consumerMutex.Lock()
+	_ = consumer.Close()
+	queue.consumerMutex.Unlock()
+
+	queue.mutex.Lock()
+	defer queue.mutex.Unlock()
+	if queue.consumer == consumer {
+		queue.consumer = nil
+		queue.consumerTag = ""
+		queue.consuming = false
+	}
+}
+
+func (queue *QueueMiddleware) consumeDeliveries(
+	deliveries <-chan amqp.Delivery,
+	callback func(m.Message, func(), func()),
+) error {
+	consumerErrors := make(chan error, 1)
+	for {
+		select {
+		case <-consumerErrors:
+			return queue.consumptionError()
+		case delivery, ok := <-deliveries:
+			if !ok {
+				return queue.consumptionError()
+			}
+
+			queue.handleDelivery(delivery, callback, consumerErrors)
+			//select por si llega un mensaje nuevo y aparte el callback falló
+			select {
+			case <-consumerErrors:
+				return queue.consumptionError()
+			default:
+			}
+		}
+	}
+}
+
+func (queue *QueueMiddleware) handleDelivery(
+	delivery amqp.Delivery,
+	callback func(m.Message, func(), func()),
+	consumerErrors chan<- error,
+) {
+	var resolution sync.Once
+	resolve := func(ack bool) {
+		resolution.Do(func() { // por si usan mal el middleware y llaman a los callbacks más de una vez
+			queue.consumerMutex.Lock()
+			var err error
+			if ack {
+				err = delivery.Ack(false)
+			} else {
+				err = delivery.Nack(false, true)
+			}
+			queue.consumerMutex.Unlock()
+		
+			if err != nil {
+				select {
+				case consumerErrors <- err:
+				default:
+				}
+			}
+		})
+	}
+
+	callback(
+		m.Message{Body: string(delivery.Body)},
+		func() { resolve(true) },
+		func() { resolve(false) },
+	)
+}
+
+func (queue *QueueMiddleware) consumptionError() error {
+	if queue.connection.IsClosed() {
 		return m.ErrMessageMiddlewareDisconnected
 	}
 	return m.ErrMessageMiddlewareMessage
